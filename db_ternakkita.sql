3571fcfe3efd749bda6e072fd7dc4f7020d5aca9
aditya37 <aditrahman212@gmail.com>
2019-09-23 14:08:53 +0700

    REST API TERNAK KITA

--- db_ternakkita.sql
+++ db_ternakkita.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 12, 2019 at 12:20 AM
+-- Generation Time: Sep 23, 2019 at 02:07 PM
 -- Server version: 10.0.38-MariaDB-0ubuntu0.16.04.1
 -- PHP Version: 7.2.22-1+ubuntu16.04.1+deb.sury.org+1
 
@@ -23,19 +23,75 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_detailProduct`
+--
+
+CREATE TABLE `tbl_detailProduct` (
+  `id_detailP` varchar(16) NOT NULL,
+  `gender` enum('Male','Female') NOT NULL,
+  `type` enum('Sapi','Kuda','Kambing','Kerbau') NOT NULL,
+  `weight` double NOT NULL,
+  `price` text NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `note` varchar(255) NOT NULL,
+  `age` int(2) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `idProduct` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_detailProduct`
+--
+
+INSERT INTO `tbl_detailProduct` (`id_detailP`, `gender`, `type`, `weight`, `price`, `description`, `note`, `age`, `image`, `idProduct`) VALUES
+('1302', 'Male', 'Sapi', 22, '1000,000', 'Sapi Lymousin No KW', '', 2, '-', '7568');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_product`
+--
+
+CREATE TABLE `tbl_product` (
+  `idProduct` varchar(16) NOT NULL,
+  `productTittle` varchar(255) NOT NULL,
+  `productCreated` date NOT NULL,
+  `productUpdate` date NOT NULL,
+  `productStatus` enum('Sold Out','Available') NOT NULL,
+  `idVendor` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_product`
+--
+
+INSERT INTO `tbl_product` (`idProduct`, `productTittle`, `productCreated`, `productUpdate`, `productStatus`, `idVendor`) VALUES
+('7568', 'Sapi Lymousin', '2019-09-12', '2019-09-19', 'Available', '1999');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_userData`
 --
 
 CREATE TABLE `tbl_userData` (
-  `id_userData` int(16) NOT NULL,
+  `id_userData` varchar(16) NOT NULL,
   `firstName` varchar(32) NOT NULL,
   `lastName` varchar(32) NOT NULL,
   `birth` date NOT NULL,
   `gender` enum('Male','Female') NOT NULL,
   `phone` text NOT NULL,
+  `locationImg` varchar(255) NOT NULL,
   `profilePhoto` varchar(255) NOT NULL,
   `id_login` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_userData`
+--
+
+INSERT INTO `tbl_userData` (`id_userData`, `firstName`, `lastName`, `birth`, `gender`, `phone`, `locationImg`, `profilePhoto`, `id_login`) VALUES
+('1111', 'Vendor', 'Test', '1998-05-02', 'Male', '081336601129', '-', '-', '1999');
 
 -- --------------------------------------------------------
 
@@ -56,11 +112,8 @@ CREATE TABLE `tbl_userLogin` (
 --
 
 INSERT INTO `tbl_userLogin` (`id_login`, `username`, `password`, `userLevel`, `dateRegister`) VALUES
-('1', '33', '33', 'Vendor', '2019-09-11'),
-('111', '333', '333', 'Vendor', '2019-02-02'),
-('1141', '34433', '333', 'Vendor', '2019-02-02'),
-('2234343', '4', '4', 'Vendor', '2019-09-11'),
-('GIusbSodEJXxACby', '44', '4', 'Customer', '2019-09-11');
+('1998', 'vendor', 'vendor', 'Vendor', '2019-09-02'),
+('1999', 'testVendor', 'vendorTest', 'Vendor', '2019-09-13');
 
 -- --------------------------------------------------------
 
@@ -69,7 +122,7 @@ INSERT INTO `tbl_userLogin` (`id_login`, `username`, `password`, `userLevel`, `d
 --
 
 CREATE TABLE `tbl_userRegion` (
-  `id_userRegion` int(11) NOT NULL,
+  `id_userRegion` varchar(16) NOT NULL,
   `administrative_area_level_1` varchar(32) NOT NULL,
   `administrative_area_level_2` varchar(32) NOT NULL,
   `administrative_area_level_3` varchar(32) NOT NULL,
@@ -80,13 +133,35 @@ CREATE TABLE `tbl_userRegion` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `tbl_userRegion`
+--
+
+INSERT INTO `tbl_userRegion` (`id_userRegion`, `administrative_area_level_1`, `administrative_area_level_2`, `administrative_area_level_3`, `administrative_area_level_4`, `address`, `postalCode`, `id_login`) VALUES
+('3522', 'Jawa Timur', 'Bojonegoro', 'Dander', 'Mojoranu', 'Jln Haryo Matahun No 113 Rt 02', '62171', '1999');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tbl_detailProduct`
+--
+ALTER TABLE `tbl_detailProduct`
+  ADD PRIMARY KEY (`id_detailP`),
+  ADD KEY `idProduct` (`idProduct`);
+
+--
+-- Indexes for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  ADD PRIMARY KEY (`idProduct`),
+  ADD KEY `idVendor` (`idVendor`);
 
 --
 -- Indexes for table `tbl_userData`
 --
 ALTER TABLE `tbl_userData`
+  ADD PRIMARY KEY (`id_userData`),
   ADD KEY `id_customer` (`id_login`);
 
 --
@@ -103,17 +178,20 @@ ALTER TABLE `tbl_userRegion`
   ADD KEY `id_login` (`id_login`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Constraints for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `tbl_userRegion`
+-- Constraints for table `tbl_detailProduct`
 --
-ALTER TABLE `tbl_userRegion`
-  MODIFY `id_userRegion` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tbl_detailProduct`
+  ADD CONSTRAINT `tbl_detailProduct_ibfk_1` FOREIGN KEY (`idProduct`) REFERENCES `tbl_product` (`idProduct`);
+
 --
--- Constraints for dumped tables
+-- Constraints for table `tbl_product`
 --
+ALTER TABLE `tbl_product`
+  ADD CONSTRAINT `tbl_product_ibfk_1` FOREIGN KEY (`idVendor`) REFERENCES `tbl_userLogin` (`id_login`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `tbl_userData`
